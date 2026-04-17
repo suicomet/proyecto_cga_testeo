bbce1d869cfc4ce2164dbd1e495264ce85797f2b
Fabrizio Javier Bugedo Gonzalez <fa.bugedo@duocuc.cl>
2026-04-16 22:50:17 -0400

    Delete build-frontend.ps1
    limpieza

--- build-frontend.ps1
+++
@@ -1,5 +0,0 @@
-# Build frontend
-$env:Path = "C:\Program Files\nodejs;$env:Path"
-cd frontend
-Write-Host "Compilando Angular..." -ForegroundColor Cyan
-npm run build -- --configuration development
